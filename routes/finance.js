af7f67798d95f218aabfa8bf12704c51c6826d0c
Darkpsychic <nairnikhil2001@gmail.com>
2019-05-17 20:48:01 +0530

    implement balance sheet statement

--- routes/finance.js
+++ routes/finance.js
@@ -1,8 +1,31 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('finance', {symbol: req.query.symbol});
+    var sym = req.query.symbol;
+
+    if(!sym)
+    {
+        res.render('finance');
+    }
+    else
+    {
+        var url = 'https://financialmodelingprep.com/api/v2/financials/income-statement/' + sym + '?datatype=json';
+        request.get(url, function(error, response, body){
+            var income_data = JSON.parse(body).financials.pop();
+            console.log(income_data);
+
+            if(income_data)
+            {
+                res.render('finance', {income_statement: income_data});
+            }
+            else
+            {
+                res.render('finance');
+            }
+        });
+    }
 });
 
 module.exports = router;
