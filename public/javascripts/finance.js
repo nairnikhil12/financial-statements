10f656f12c58bea75dfb67ca2e168df046255dde
Darkpsychic <nairnikhil2001@gmail.com>
2019-05-18 11:38:00 +0530

    implement tab styling

--- public/javascripts/finance.js
+++ public/javascripts/finance.js
@@ -12,17 +12,30 @@ window.onload = function(){
         inc_statement.style.display = "block";
         bal_statement.style.display = "none";
         cf_statement.style.display = "none";
+        
+        income_btn.style.color = "#f44336";
+        balance_btn.style.color = "#000000";
+        cashflow_btn.style.color = "#000000";
     }
 
     balance_btn.onclick = function(){
         inc_statement.style.display = "none";
         bal_statement.style.display = "block";
         cf_statement.style.display = "none";
+
+        income_btn.style.color = "#000000";
+        balance_btn.style.color = "#f44336";
+        cashflow_btn.style.color = "#000000";
     }
 
     cashflow_btn.onclick = function(){
         inc_statement.style.display = "none";
         bal_statement.style.display = "none";
         cf_statement.style.display = "block";
+        
+        income_btn.style.color = "#000000";
+        balance_btn.style.color = "#000000";
+        cashflow_btn.style.color = "#f44336";
+
     }
 }
