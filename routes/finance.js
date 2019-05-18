4b099e284e660c5fea354fa713702b700f08cced
Darkpsychic <nairnikhil2001@gmail.com>
2019-05-18 08:41:48 +0530

    add retrieving all income statements

--- routes/finance.js
+++ routes/finance.js
@@ -11,19 +11,37 @@ router.get('/', function(req, res){
     }
     else
     {
-        var url = 'https://financialmodelingprep.com/api/v2/financials/income-statement/' + sym + '?datatype=json';
-        request.get(url, function(error, response, body){
-            var income_data = JSON.parse(body).financials.pop();
-            console.log(income_data);
+        var url_income = 'https://financialmodelingprep.com/api/v2/financials/income-statement/' + sym + '?datatype=json';
+        var url_balance = 'https://financialmodelingprep.com/api/v2/financials/balance-sheet-statement/' + sym + '?datatype=json';
+        var url_cashflow = 'https://financialmodelingprep.com/api/v2/financials/cash-flow-statement/' + sym + '?datatype=json';
+        var income_data;
+        var balance_data;
+        var cash_flow;
 
-            if(income_data)
-            {
-                res.render('finance', {income_statement: income_data});
-            }
-            else
-            {
-                res.render('finance');
-            }
+        var promise_income = new Promise(function(resolve, reject){
+            request.get(url_income, function(error, response, body){
+                income_data = JSON.parse(body).financials.pop();
+                resolve();
+            });
+        });
+
+        var promise_balance = new Promise(function(resolve, reject){
+            request.get(url_balance, function(error, response, body){
+                balance_data = JSON.parse(body).financials.pop();
+                resolve();
+            });
+        });
+            
+        var promise_cashflow = new Promise(function(resolve, reject){
+            request.get(url_cashflow, function(error, response, body){
+                resolve();
+                cash_flow = JSON.parse(body).financials.pop();
+            });
+        });
+
+        Promise.all([promise_income, promise_balance, promise_cashflow]).then(function(){
+            console.log(balance_data);
+            res.render('finance', {income_statement: income_data, balance_statement: balance_data, cashflow_statement: cash_flow});
         });
     }
 });
